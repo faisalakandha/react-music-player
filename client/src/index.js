77662b4f7ddcf57e909ab0ccadfbd9d99e0f7f52
faisalakandha <57830958+faisalakandha@users.noreply.github.com>
2022-02-19 03:16:17 +0600

    (chore): routes fix

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import MusicPlayer from './components/player/App';
 
 import './index.scss';
+
 
 ReactDOM.render(    
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />}>
-{/*      <Route path="track/:id" element={MusicPlayer} /> */}
+      <Route path="/" element={<App />} />
+      <Route path="/music-player" element={<MusicPlayer />}> 
       </Route>
     </Routes>
   </BrowserRouter>,
